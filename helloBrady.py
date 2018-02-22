26d15dda5f8ffc6a51c46ab8920b86fabef9350a
bradyvall <brady.vallbacka@mail.utoronto.ca>
2018-02-22 12:03:18 -0500

    helloBrady.py now utilizes testfile.html to test and html file on server successfully

--- helloBrady.py
+++ helloBrady.py
@@ -1,8 +1,12 @@
 # this is a test commit from Brady
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello Bill I am committing this and testing the server.'
+    return render_template('testfile.html')
+
+
+if __name__ == '__main__':
+    app.run()
