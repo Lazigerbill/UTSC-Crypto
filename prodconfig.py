76c069f1652454d29eb6f9c02f2567fff35a67d0
bradyvall <brady.vallbacka@mail.utoronto.ca>
2018-04-17 13:30:45 -0400

    Added keys for forms to Progfile.

--- prodconfig.py
+++ prodconfig.py
@@ -6,3 +6,5 @@ class ProductionConfig:
     MYSQL_DATABASE_HOST = os.environ['MYSQL_DATABASE_HOST']
     MYSQL_DATABASE_PASSWORD = os.environ['MYSQL_DATABASE_PASSWORD']
     MYSQL_DATABASE_USER = os.environ['MYSQL_DATABASE_USER']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    WTF_CSRF_SECRET_KEY = ['acsrfsecretkey']
