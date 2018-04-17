aaf37e1d8ad4f406b98105ee514fbd5a4f2ae67b
bradyvall <brady.vallbacka@mail.utoronto.ca>
2018-04-17 14:01:39 -0400

    Found another error.

--- prodconfig.py
+++ prodconfig.py
@@ -7,4 +7,4 @@ class ProductionConfig:
     MYSQL_DATABASE_PASSWORD = os.environ['MYSQL_DATABASE_PASSWORD']
     MYSQL_DATABASE_USER = os.environ['MYSQL_DATABASE_USER']
     SECRET_KEY = os.environ['SECRET_KEY']
-    WTF_CSRF_SECRET_KEY = os.environ['acsrfsecretkey']
+    WTF_CSRF_SECRET_KEY = os.environ['WTF_CSRF_SECRET_KEY']
